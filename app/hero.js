29ebe0ef46c4efdb0cd08fd8e5b7932ed00088fb
cutofmyjib <thedianalozano@gmail.com>
2016-02-09 15:12:56 -0800

    add container

--- app/hero.js
+++ app/hero.js
@@ -3,12 +3,14 @@ import React, {Component} from 'react';
 export default class Hero extends Component {
   render() {
     return (
-      <section className="hero">
-        <h1>Hero Title</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <a className="button-link-dark">Call to action</a>
-      </section>
+      <div className="hero">
+        <div className="center-container">
+          <h1>Hero Title</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a className="button-link-dark">Call to action</a>
+        </div>
+      </div>
     );
   }
 }
