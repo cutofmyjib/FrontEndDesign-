609ef21a2d44d316a12dde39c1fca2fe98195dfc
cutofmyjib <thedianalozano@gmail.com>
2016-02-09 15:12:29 -0800

    add images component and styling

--- app/App.js
+++ app/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Header from './header.js'
 import Hero from './hero.js'
+import Images from './images.js'
 import Footer from './footer.js'
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Header />
         <Hero />
+        <Images />
         <Footer />
       </div>
     );
---
+++ app/images.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+export default class Images extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="image-list">
+          <div className="img-div">
+            <img src="http://placehold.it/350x150" />
+          </div>
+          <div className="p-div">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+  sed do eiusmod tempor incididunt ut labore et dolore
+  magna aliqua. Ut enim ad minim veniam, quis nostrud
+  exercitation ullamco laboris nisi ut aliquip ex eatur.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
