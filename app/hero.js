63f31a5ce208ec1411ff448bb65a30c2d415638e
cutofmyjib <thedianalozano@gmail.com>
2016-02-10 02:25:38 -0800

    change class name

--- app/hero.js
+++ app/hero.js
@@ -5,10 +5,10 @@ export default class Hero extends Component {
     return (
       <div className="hero">
         <div className="center-container hero-wrapper">
-          <h1>Hero Title</h1>
+          <h1 className="heading">Hero Title</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
   eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <a className="button-link-dark">Call to action</a>
+          <a className="button-link-hero">Call to action</a>
         </div>
       </div>
     );
